8fe9490b6f6b9166cfd4ec35189e42f5e6a30e22
Aziz Fikri <afikrim10@gmail.com>
2020-11-18 08:59:29 +0700

    add auth user

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Firebase\JWT\JWT;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register (Request $request)
+    {
+        $this->validate($request, [
+            'username' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $user = new User;
+        $user->username = $request->username;
+        $user->password = Hash::make($request->password);
+
+        $jwt = JWT::encode($user, env('JWT_SECRET'));
+
+        try {
+            $user->save();
+
+            return response()->json([
+                'success' => true,
+                'data' => [
+                    'token' => $jwt
+                ]
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'data' => null
+            ], 500);
+        }
+    }
+
+    public function login (Request $request)
+    {
+        $this->validate($request, [
+            'username' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $user = User::where('username', $request->username)->first();
+
+        if (Hash::check($request->password, $user->password)) {
+            $jwt = JWT::encode($user, env('JWT_SECRET'));
+
+            return response()->json([
+                'success' => true,
+                'data' => [
+                    'token' => $jwt
+                ]
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Password Salah!'
+            ]);
+        }
+    }
+}
